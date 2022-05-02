403f22dbc6481b43164d52e2e05a9cf07b78b80d
unknown <akbiyik.ilker7@gmail.com>
2022-05-02 12:59:31 +0200

    minor change

--- src/app/test-component/test-component.component.ts
+++ src/app/test-component/test-component.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+type User = {id: Number, name: string}
+
 @Component({
   selector: 'app-test-component',
   templateUrl: './test-component.component.html',
   styleUrls: ['./test-component.component.sass']
 })
 export class TestComponentComponent implements OnInit {
-
+  users: User[] = [];
+  currentUser: User | undefined;
   constructor() { }
 
   ngOnInit(): void {
